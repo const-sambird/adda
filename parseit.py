2e346933f61199f1e3f214adb3e83d6089a36749
Sam Bird <sam@sambird.dev>
2026-07-17 13:22:24 -0500

    failure parsing

--- parseit.py
+++ parseit.py
@@ -16,10 +16,24 @@ if __name__ == '__main__':
     for i in range(1, n_runs + 1):
         with open(f'./{prefix}_{i}.log', 'r') as infile:
             lines = infile.readlines()
-            config = lines[-4].split(' ')
+            config = lines[1].split(' ')
             config[-1].strip('\n')
             config = '\n'.join(config)
-            routes = lines[-2]
+            routes = lines[2]
+
+            if len(lines) > 5:
+                n_reps = len(lines) // 5
+                for j in range(1, n_reps):
+                    f_config = lines[(j * 5) + 1].split(' ')
+                    f_config[-1].strip('\n')
+                    f_config = '\n'.join(f_config)
+                    f_routes = lines[(j * 5) + 2]
+
+                    with open(f'./configs/{prefix}_{i}_fail{j-1}.csv', 'a') as cf_outfile:
+                        cf_outfile.writelines(f_config)
+                    with open(f'./routes/{prefix}_{i}_fail{j-1}.csv', 'a') as rf_outfile:
+                        rf_outfile.write(f_routes.strip('\n'))
+
 
             with open(f'./configs/{prefix}_{i}.csv', 'a') as c_outfile:
                 c_outfile.writelines(config)
